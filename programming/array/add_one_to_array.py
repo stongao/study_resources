0eb386273c125c7a72b6cce70a8d8f22573ad537
stongao <tongaonkarsushant@gmail.com>
2021-04-03 21:30:27 -0700

    var names

--- programming/array/add_one_to_array.py
+++ programming/array/add_one_to_array.py
@@ -4,15 +4,18 @@ class Solution:
     # @return a list of integers
     def plusOne(self, A):
         carry = 1
-        for i in range(len(A) -1, -1, -1):
-            t = A[i]
-            A[i] = (t + carry) % 10
-            carry = (t + carry) // 10
+        loop_start_last_elem_index = len(A) - 1
+        loop_end_index_minus_one = -1
+        loop_direction = -1
+        for elem_index in range(loop_start_last_elem_index, loop_end_index_minus_one, loop_direction):
+            elem = A[elem_index]
+            A[elem_index] = (elem + carry) % 10
+            carry = (elem + carry) // 10
         if carry:
-            A.insert(0,1)
-        i = 0
-        while i < len(A)-1:
-            if A[i] != 0:
+            A.insert(0, 1)
+        elem_index = 0
+        while elem_index < len(A)-1:
+            if A[elem_index] != 0:
                 break
-            i = i+1
-        return A[i:]
+            elem_index = elem_index+1
+        return A[elem_index:]
